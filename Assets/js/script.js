16d91a91f4f71c3595e644558583a6d516d955a6
Jen Ryt <jenyjlin@gmail.com>
2023-03-12 03:11:46 -0700

    added major code

---
+++ Assets/js/script.js
@@ -0,0 +1,181 @@
+const container = document.querySelector(".container");
+const startScreen = document.querySelector(".startScreen");
+const startbnt = document.querySelector("#start");
+const timerEl = document.querySelector(".timer");
+
+const qScreen = document.querySelector(".qScreen");
+const qTitle = document.querySelector(".qTitle");
+const choice1 = document.querySelector(".one");
+const choice2 = document.querySelector(".two");
+const choice3 = document.querySelector(".three");
+const choice4 = document.querySelector(".four");
+const answerMsg = document.querySelector(".answerMsg");
+
+const endScreen = document.querySelector(".endScreen");
+const displayScore = document.querySelector('.showScore');
+const userInitial = document.querySelector('#userInitial');
+
+let qIndex = 0;
+
+let quizTime = 100;
+let timeInt;
+
+qScreen.style.display = 'none';
+endScreen.style.display = 'none';
+// When the start button is clicked
+startbnt.addEventListener('click', beginQuiz)
+
+function beginQuiz() {
+  startScreen.style.display = "none";
+  qScreen.style.display = 'block';
+  myTimer();
+  navQuiz();
+}
+
+// Navigate between questions
+function navQuiz() {
+  console.log("nav quiz", qIndex);
+  if (qIndex == 0 || qIndex < questions.length){
+    qTitle.textContent = questions[qIndex].question;
+    choice1.textContent = questions[qIndex].options[0];
+    choice2.textContent = questions[qIndex].options[1];
+    choice3.textContent = questions[qIndex].options[2];
+    choice4.textContent = questions[qIndex].options[3];
+    console.log('test1', questions[qIndex].answer)
+    qIndex = qIndex + 1;
+    console.log('test2', questions[qIndex-1].answer)
+    } else {
+      quizEnd();
+    }
+}
+
+// eventListener for when to trigger question navigation function
+container.addEventListener('click', function(event){
+  const element = event.target;
+    if(element.matches('.choice')){
+      console.log('test3', `click on ${element.innerHTML}`, `answer is ${questions[qIndex-1].answer}`); //<---
+      if (element.innerHTML !== questions[qIndex-1].answer) {
+        quizTime -= 10; //subtrack time 
+        answerMsg.textContent = "Wrong!";
+        navQuiz();
+      } else{
+        answerMsg.textContent = "Correct!";
+        navQuiz();
+      }
+    }
+});  
+  
+
+function myTimer() {
+  timeInt = setInterval(function() {
+    quizTime--;
+    timerEl.textContent = `${quizTime} seconds left`;
+    if(quizTime === 0){
+      clearInterval(timeInt);
+      quizEnd();
+    }
+  }, 1000);
+}
+
+function quizEnd() {
+  qScreen.style.display = 'none';
+  timerEl.style.display = 'none';
+  endScreen.style.display = 'block';
+  displayScore.textContent = 'your score';
+  // userInitial.style.display = 'block';
+}
+
+
+
+// 11 questions with answer: objects in an array
+  const questions = [
+    {
+      question: 'What is the capital of Taiwan?',
+      options: [
+        'Taipei',
+        'Taichung',
+        'Hong Kong',
+        'Shanghai'], answer: 'Taipei'
+    },
+    {
+      question: 'What is the capital of Ukraine?',
+      options: [
+        'Luhansk',
+        'Donetsk',
+        'Kyiv',
+        'Odesa'], answer: 'Kyiv'
+    },
+    {
+      question: 'What is the capital of Japan?',
+      options: [
+        'Osaka',
+        'Tokyo',
+        'Sapporo',
+        'Kyoto'], answer: 'Tokyo'
+    },
+    {
+      question: 'What is the capital of South Korea?',
+      options: [
+        'Busan',
+        'Seoul',
+        'Jeju-si',
+        'Incheon'], answer: 'Seoul'
+    },
+    {
+      question: 'What is the capital of France?',
+      options: [
+        'Nice',
+        'Marseille',
+        'Lyon',
+        'Paris'], answer: 'Paris'
+    },
+    {
+      question: 'What is the capital of Egypt?',
+      options: [
+        'Cairo',
+        'Alexandria',
+        'Giza',
+        'Faiyum'], answer: 'Cairo'
+    },
+    {
+      question: 'What is the capital of Australia?',
+      options: [
+        'Canberra',
+        'Sydney',
+        'Brisbane',
+        'Perth'], answer: 'Canberra'
+    },
+    {
+      question: 'What is the capital of Argentina?',
+      options: [
+        'Mendoza',
+        'Buenos Aires',
+        'Córdoba',
+        'Rosario'], answer: 'Buenos Aires'
+    },
+    {
+      question: 'What is the capital of Italy?',
+      options: [
+        'Venice',
+        'Florence',
+        'Rome',
+        'Naples'], answer: 'Rome'
+    },
+    {
+      question: 'What is the capital of Germany?',
+      options: [
+        'Munich',
+        'Hamburg',
+        'Berlin',
+        'Frankfurt'], answer: 'Berlin'
+    },
+    {
+      question: 'What is the capital of United States?',
+      options: [
+        'Chicago',
+        'San Francisco',
+        'New York',
+        'Washington, D.C.'], answer: 'Washington, D.C.'
+    }
+  ]
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TimedQuiz
-creating a timed quiz
+
+Working as an individual to create a timed quiz.
+
+* As a developer, I want to create a timed quiz to JavaScript fundamentals that stores high scores
+
+## Acceptance Criteria
+
+* It is done when users click on the start button, the timer starts and the first question is presented.
+* It is done when time is subtracted from the clock. 
+* It is done when the next question is presented when users answer the current one.
+* It is done when a question is answered incorrectly, the users are presented with another question.
+* It is done when all questions are answered or when the timer reaches 0, the quiz is over.
+* It is done when users’ score are presented after the quiz ends.
+* It is done when users are given the choice to save their initials and score after the quiz ends.
+## Assets
+
+The following link is to the website for password generator. 
+https://jenryt.github.io/TimedQuiz/
